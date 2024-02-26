5701f1ca317780b0d2635b1df2ffb1bdabe992b2
happysubin <asb0711@gmail.com>
2024-02-26 20:17:51 +0900

    react.js styled component 복습

--- react-playground/playground/src/App.js
+++ react-playground/playground/src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components"
+
+const Father = styled.div`
+  display: flex;
+`
+
+const Box = styled.div`
+  background-color: ${(props) => props.bgColor};
+  width: 100px;
+  height: 100px;
+`
+
+const BoxOne = styled.div`
+  background-color: teal;
+  width: 100px;
+  height: 100px;
+`
+
+const BoxTwo = styled.div`
+  background-color: tomato;
+  width: 100px;
+  height: 100px;
+`
+
+const Text = styled.span`
+  color: white;
+`
+
+const Circle = styled(Box)`
+  border-radius: 50px;
+`
+
+const Button = styled.button`
+  color: white;
+  background-color: tomato;
+  border: 0;
+  border-radius: 15px;
+`
+
+const Link = styled(Button)``
+
+const Input = styled.input.attrs({required:true, minLength:10 })`
+  background-color: blue;
+`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Father as = "header">
+      <Box bgColor="teal">
+        <Text>Hello</Text>
+      </Box>
+      <Circle bgColor="tomato"/>
+      <Button as="a" href="/">Login</Button>  //a 태그로 활용됨
+      <Input></Input>
+    </Father>
+  )
+ 
 }
 
 export default App;
